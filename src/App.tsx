431658dbcf737aecd2c372c8ed1530ed0fa94d73
Tijmen Verhoef <tijmenmenno@gmail.com>
2025-10-18 17:29:02 +0200

    updates landing page with WIP notice

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Tijmen Verhoef's Portfolio Website</h1>
+      <h1>Tijmen Verhoef's Portfolio Website (WIP)</h1>
+      <p>This page will serve a portfolio website containing all my programming projects.</p>
     </>
   )
 }
