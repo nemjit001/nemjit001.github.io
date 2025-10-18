b41852fd07651c8f415a3119d19539472852f620
Tijmen Verhoef <tijmenmenno@gmail.com>
2025-10-18 18:16:59 +0200

    adds basic app layout w/ placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Tijmen Verhoef's Portfolio Website (WIP)</h1>
+      <h1>Tijmen Verhoef's Portfolio Website</h1>
       <p>This page will serve a portfolio website containing all my programming projects.</p>
+      <h2>About me</h2>
+      <p>Who am I, what am I, what am I working on?</p>
+      <h2>Projects</h2>
+      <p>Some projects here...</p>
     </>
   )
 }
