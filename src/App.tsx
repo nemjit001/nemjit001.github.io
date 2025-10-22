b47b243c7d42eb4cc7495654a9d355649ed75cfa
Tijmen Verhoef <tijmenmenno@gmail.com>
2025-10-22 13:14:49 +0200

    moves AboutMe to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
+import AboutMe from './App/AboutMe'
 
 function App() {
   return (
     <>
       <h1>Tijmen Verhoef's Portfolio Website</h1>
       <p>This page will serve a portfolio website containing all my programming projects.</p>
-      <h2>About me</h2>
-      <p>Who am I, what am I, what am I working on?</p>
-      <h2>Projects</h2>
-      <p>Some projects here...</p>
+      <AboutMe />
     </>
   )
 }
---
+++ src/App/AboutMe.tsx
@@ -0,0 +1,10 @@
+function AboutMe() {
+    return (
+        <>
+            <h2>Who am I?</h2>
+            <p>Something interesting here</p>
+        </>
+    );
+}
+
+export default AboutMe;
